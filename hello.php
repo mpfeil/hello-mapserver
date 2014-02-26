15a214bb592d56ed4f3b2a12ac4763983b20220e
Matthias Pfeil <matthias.pfeil@wwu.de>
2014-02-26 16:50:18 +0000

    added gitignore

--- hello.php
+++ hello.php
@@ -1,18 +1,31 @@
 <?php
-	$map = new mapObj("/var/www/2213/chapter02/DEU_adm1.map");
 
+	$map = new mapObj("/var/www/2213/chapter02/DEU_adm1.map");
+	$layer = $map->getLayerByName('DEU_adm1');
 	$layers = $map->getAllLayerNames();
 
-	$image=$map->draw();
-    $image_url=$image->saveWebImage();
+	if (isset($_POST["submitStyle"])) {
 
-    $layer = $map->getLayerByName('DEU_adm1');
+		$startColor = hex2rgb($_POST["startColor"]);
+		$endColor = hex2rgb($_POST["endColor"]);
 
-	$featuresInLayer = getNumOfFeatures($map,$layer);
-	
-	$colors = getColors(array(128,175,27),array(255,255,140),$featuresInLayer);
+		$field = "";
 
-	generateCategorizedStyle($map,$layer,$featuresInLayer);
+		if (isset($_POST["field"])) {
+			$field = $_POST["field"];
+		} else {
+			$field = "NAME_1";
+		}
+
+		$featuresInLayer = getNumOfFeatures($map,$layer,$field);
+
+		$colors = getColors(array($startColor[0],$startColor[1],$startColor[2]),array($endColor[0],$endColor[1],$endColor[2]),count($featuresInLayer));
+
+		generateCategorizedStyle($map,$layer,$field,$featuresInLayer,$colors);
+
+		$image=$map->draw();
+    	$image_url=$image->saveWebImage();
+	}
 
 	//Generates an array of colors for a colorramp and a number of features
 	function getColors($startColor, $endColor, $count) {
@@ -31,49 +44,67 @@
 		return $resultArray;	
 	}
 
-	function generateCategorizedStyle($map,$layer,$featuresInLayer) {
+	function generateCategorizedStyle($map,$layer,$field,$featuresInLayer,$colors) {
 		
 		//remove all classes for layer
-		for ($i=0; $i<$layer->numclasses; $i++)
-		{
-		    $oClass = $layer->getClass($i);
-		    $layer->removeClass($i);
+		while ($layer->numclasses > 0) {
+		    $layer->removeClass(0);
 		}
 
-		//TODO iterate over all colors and features and generate classes and styles
-
 		//create classObject (set Name(Layername), set Expression(filter for different styling))
-		$class = new classObj($layer);
-		$class->set("name","Berlin");
-		$class->setExpression("('[NAME_1]' = 'Berlin')");
-		
-		//create styleObject
-		$style = new styleObj($class);
-		$style->color->setRGB(128,175,27);
-		$style->outlinecolor->setRGB(0,0,0);
-		$style->width = 0.26;
-
+		for ($i=0; $i < count($featuresInLayer); $i++) {
+			$class = new classObj($layer);
+			$class->set("name",$featuresInLayer[$i]);
+			$class->setExpression("('[$field]' = '$featuresInLayer[$i]')");
+			
+			//create styleObject
+			$style = new styleObj($class);
+			$style->color->setRGB($colors[$i][0],$colors[$i][1],$colors[$i][2]);
+			$style->outlinecolor->setRGB(0,0,0);
+			$style->width = 0.26;
+		}
 		//save map
 		$map->save($map->mappath . "DEU_adm1.map");
 	}
 
-	function getNumOfFeatures($map,$layer) {
-		$result = 0;
+	function getNumOfFeatures($map,$layer,$field) {
+		$resultArray = array();
 
 		$status = $layer->open();
 		$status = $layer->whichShapes($map->extent);	
 		while ($shape = $layer->nextShape())
 		{
-			$result++;
+			if (!in_array($shape->values[$field], $resultArray)) {
+				array_push($resultArray, $shape->values[$field]);
+			}
 		}
 		$layer->close();
 
-		return $result;
+		return $resultArray;
+	}
+
+	//Convert hex color code to rgb
+	function hex2rgb($hex) {
+	   $hex = str_replace("#", "", $hex);
+
+	   if(strlen($hex) == 3) {
+	      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+	      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+	      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+	   } else {
+	      $r = hexdec(substr($hex,0,2));
+	      $g = hexdec(substr($hex,2,2));
+	      $b = hexdec(substr($hex,4,2));
+	   }
+	   $rgb = array($r, $g, $b);
+	   //return implode(",", $rgb); // returns the rgb values separated by commas
+	   return $rgb; // returns an array with the rgb values
 	}
 ?>
 <html>
 	<head>
 		<title>First PHP/MapScript Action</title>
+		<script type="text/javascript" src="vendor/jscolor/jscolor.js"></script>
 	</head>
 	<body>
 		<h1>Layers</h1>
@@ -85,10 +116,8 @@
 					}	
 				?>
 			</select>
-			<input type="submit">
+			<input type="submit" name="submitLayers">
 		</form>
-		<hr>
-			<IMG SRC=<?php echo $image_url; ?> >
 		<hr>
 		<h1>Style</h1>
 		<form name="style" action="hello.php" method="POST">
@@ -97,30 +126,31 @@
 				<option value="categorizedSymbol">Categorized Symbol</option>
 				<option value="graduatedSymbol">Graduated Symbol</option>	
 			</select>
-			<input type="submit">
+			<br />
+			<select name="field">
+				<?php
+					if (isset($_POST['submitLayers'])) {	
+						$layer = $map->getLayerByName("DEU_adm1");
+
+						//open layer to work with it
+						$status = $layer->open();
+						echo $status;
+
+						//read all attributes of layer
+						$attributes = $layer->getItems();
+						
+						foreach ($attributes as $key => $value) {
+							echo "<option value='$value'>". $value . "</option>";
+						}
+					} 
+				?>
+			</select>
+			<br />
+			<input name="startColor" class="color"> - <input name="endColor" class="color">
+			<input type="submit" name="submitStyle">
 		</form>
 		<hr>
-		<h1>Fields</h1>
-		<select name="fields">
-			<?php
-				// if (isset($_POST['cbLayers'])) {
-					// $layer = $map->getLayerByName($_POST['cbLayers']);	
-					$layer = $map->getLayerByName("DEU_adm1");
-
-
-					//open layer to work with it
-					$status = $layer->open();
-					echo $status;
-
-					//read all attributes of layer
-					$attributes = $layer->getItems();
-					
-					foreach ($attributes as $key => $value) {
-						echo "<option value='$key'>" . $key . ":" . $value . "</option>";
-					}
-				// } 
-			?>
-		</select>
+			<IMG SRC=<?php echo $image_url; ?> >
 		<hr>
 	</body>
 </html>
