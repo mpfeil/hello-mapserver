4ed97db927044422254a90d06209c6d541c24bb1
Matthias Pfeil <matthias.pfeil@wwu.de>
2014-03-03 17:56:07 +0000

    natural jenks implementation

--- hello.php
+++ hello.php
@@ -80,6 +80,8 @@
 		$status = $layer->whichShapes($map->extent);	
 		while ($shape = $layer->nextShape())
 		{
+			print_r($shape->values);
+			echo "<br />";
 			if (!in_array($shape->values[$field], $resultArray)) {
 				array_push($resultArray, $shape->values[$field]);
 			}
@@ -168,6 +170,142 @@
 		//save map
 		$map->save($map->mappath . "DEU_adm1.map");
 	}
+
+	function jenks($data, $n_classes) {
+
+		// Compute the matrices required for Jenks breaks. These matrices
+    	// can be used for any classing of data with `classes <= n_classes`
+		function getMatrices($data,$n_classes) {
+
+			// in the original implementation, these matrices are referred to
+	        // as `LC` and `OP`
+	        //
+	        // * lower_class_limits (LC): optimal lower class limits
+	        // * variance_combinations (OP): optimal variance combinations for all classes
+			$lower_class_limits = array();
+			$variance_combinations = array();
+			// loop counters
+			$i;
+			$j;
+			// the variance, as computed at each step in the calculation
+			$variance = 0;
+
+			// Initialize and fill each matrix with zeroes
+			for ($i=0; $i < count($data)+1; $i++) { 
+				$tmp1 = array();
+				$tmp2 = array();
+				for ($j=0; $j < $n_classes + 1; $j++) { 
+					array_push($tmp1, 0);
+					array_push($tmp2, 0);
+				}
+				array_push($lower_class_limits, $tmp1);
+				array_push($variance_combinations, $tmp2);
+			}
+
+			for ($i=1; $i < $n_classes + 1; $i++) { 
+				$lower_class_limits[1][$i] = 1;
+				$variance_combinations[1][$i] = 0;
+				// in the original implementation, 9999999 is used but
+            	// since PHP has `Infinity`, we use that.
+				for ($j=2; $j < count($data)+1; $j++) { 
+					$variance_combinations[$j][$i] = Infinity;
+				}
+			}
+			// print_r($lower_class_limits);
+			// echo "<br />";
+			// print_r($variance_combinations);
+			// echo "<br />";
+
+			for ($l=2; $l < count($data)+1 ; $l++) { 
+				$sum = 0;
+				$sum_squares = 0;
+				$w = 0;
+				$i4 = 0;
+
+				for ($m=1; $m < $l + 1 ; $m++) { 
+					$lower_class_limit = $l - $m + 1;
+					// echo "Lower__Class_Limit: " . $lower_class_limit . "<br />";
+					$val = $data[$lower_class_limit - 1];
+					// echo "Val" . $val ."<br />";
+
+					$w++;
+
+					$sum += $val;
+					$sum_squares += $val * $val;
+
+					$variance = $sum_squares - ($sum * $sum) / $w;
+					// echo $variance . "<br />";
+					$i4 = $lower_class_limit - 1;
+					// echo $i4 . "<br />";
+
+					if ($i4 !== 0) {
+						for ($j=2; $j < $n_classes + 1; $j++) {
+							echo $variance_combinations[$l][$j] . "<br />";
+							echo $variance + $variance_combinations[$i4][$j-1] . "<br />"; 
+							if ($variance_combinations[$l][$j] >= ($variance + $variance_combinations[$i4][$j-1])) {
+								echo "bigger <br/>";
+								$lower_class_limits[$l][$j] = $lower_class_limit;
+								$variance_combinations[$l][$j] = $variance + $variance_combinations[$i4][$j-1];
+							}
+						}
+					}
+				}
+
+				$lower_class_limits[$l][1] = 1;
+            	$variance_combinations[$l][1] = $variance;
+			}
+			return array(
+				"lower_class_limits"=>$lower_class_limits,
+				"variance_combinations"=>$variance_combinations
+			);
+		}
+
+		function breaks($data, $lower_class_limits, $n_classes) {
+			$k = count($data) - 1;
+			$kclass = array();
+			$countNum = $n_classes;
+
+			// the calculation of classes will never include the upper and
+	        // lower bounds, so we need to explicitly set them
+	        $kclass[$n_classes] = $data[count($data)-1];
+	        $kclass[0] = $data[0];
+
+	        // the lower_class_limits matrix is used as indexes into itself
+        	// here: the `k` variable is reused in each iteration.
+	        while ($countNum > 1) {
+	        	$kclass[$countNum-1] = $data[$lower_class_limits[$k][$countNum]-2];
+	        	$k = $lower_class_limits[$k][$countNum]-1;
+	        	$countNum--;
+	        }
+	 
+	        return $kclass;
+		}
+
+		if ($n_classes > count($data)) {
+			return null;
+		}
+
+		//sort data in numerical order, expected by matrices function 
+		sort($data);
+
+		// get our basic matrices
+    	$matrices = getMatrices($data, $n_classes);
+    	// echo "MATRICES <br />";
+    	// print_r($matrices["lower_class_limits"]);
+     //    echo "<br />";
+
+        // we only need lower class limits here
+        $lower_class_limits = $matrices["lower_class_limits"];
+        // echo "lower_class_limits <br />";
+        // print_r($lower_class_limits);
+ 
+    	// extract n_classes out of the computed matrices
+    	//echo "BREAKS <br />";
+    	print_r(breaks($data,$lower_class_limits,$n_classes));
+    	// return breaks($data, $lower_class_limits, $n_classes);
+	}
+
+	jenks(array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16),5);
 ?>
 <html>
 	<head>
@@ -182,6 +320,34 @@
 					foreach($layers as $layer) {
 						echo "<option value='$layer'>" . $layer . "</option>";
 					}	
+				?>
+			</select>
+			<select name="test">
+				<?php 
+					$layer = $map->getLayerByName('DEU_adm1');
+					$status = $layer->open();
+
+					//read all attributes of layer
+					$attributes = $layer->getItems();
+
+					//iterate over layer attribtues	
+					foreach ($attributes as $key => $value) {
+						$isNumeric = false;
+						//get shapes from layer and look up attribute values
+						$status = $layer->whichShapes($map->extent);
+						while ($shape = $layer->nextShape())
+						{
+							if (is_numeric($shape->values["$value"])) {
+								$isNumeric = true;
+							} else {
+								$isNumeric = false;
+								break 1;
+							}
+						}
+						if ($isNumeric) {
+							echo "<option value='$value'>" . $value . "</option>";
+						}
+					}
 				?>
 			</select>
 			<input type="submit" name="submitLayers">
@@ -202,7 +368,6 @@
 
 						//open layer to work with it
 						$status = $layer->open();
-						echo $status;
 
 						//read all attributes of layer
 						$attributes = $layer->getItems();
